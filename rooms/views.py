3c220b1e4ca53dd0c1f7e423affb5606467d54b5
timotomate <masonmount323@gmail.com>
2022-02-01 15:55:03 +0900

    #13.0

--- rooms/views.py
+++ rooms/views.py
@@ -1,10 +1,8 @@
-from datetime import datetime
 from django.shortcuts import render
-from django.http import HttpResponse
+from . import models
 
 
 # Create your views here.
 def all_rooms(request):
-    now = datetime.now()
-    #return HttpResponse(content=f"{now}<h1>Hello Sucker</h1>")
-    return render(request, "all_rooms")
+    all_rooms = models.Room.objects.all()
+    return render(request, "rooms/home.html", context = {"rooms":all_rooms})
