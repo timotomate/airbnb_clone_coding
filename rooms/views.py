6feb2f7eeaf3375cc252b02b8d70b4713f15c76f
timotomate <masonmount323@gmail.com>
2022-02-14 19:48:28 +0900

    #11.6 Pagination by Django

--- rooms/views.py
+++ rooms/views.py
@@ -1,26 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from math import ceil #최소 정수 출력 함수
 from . import models
-
+from django.core.paginator import Paginator,EmptyPage
 
 # Create your views here.
 def all_rooms(request):
-    page = int(request.GET.get("page",  1))
-    page = int(page or 1)
-    print(request.GET)# 터미널 창에서 확인용
-    print(request.GET.get("page"))# 터미널 창에서 확인용
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-    return render(
-        request,
-        "rooms/home.html",
-        {
-            "potato": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count),
-        },
-    )
+    page = request.GET.get("page", 1)
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=3)
+    #rooms = paginator.get_page(page)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page" : rooms})
+    except Exception:
+        return redirect("/")
