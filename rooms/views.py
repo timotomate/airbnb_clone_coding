789cceab7c97b031b4771a7b25aea5a560717f86
timotomate <masonmount323@gmail.com>
2022-02-17 17:53:18 +0900

    #13.3

--- rooms/views.py
+++ rooms/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, DetailView
 from django.http import Http404
 from django.urls import reverse
 from django.shortcuts import render, redirect
+from django_countries import countries
 from . import models
 
 
@@ -31,9 +32,43 @@ class RoomDetail(DetailView):
 
 
 def search(request):
-    city = request.GET.get("city")
+    city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
-    return render(request, "rooms/search.html", {"city" : city})
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    s_amenities = request.GET.get("amenities")
+    s_facilities = request.GET.get("facilities")
+    
+    
+    form = {
+        "city": city,
+        "s_room_type": room_type,
+        "s_country": country,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+
+    return render(request, "rooms/search.html", {**form, **choices})
     # {"city" : city}는 context를 의미함. context는 현재파일인 views.py에서 변수로 사용한 city 변수를 html 변수로 넘기는 것을 의미함
     # render 함수 = template인 html파일을 화면에 띄워줌
     # 파리미터 = request, template, 
+    # countries변수는 django.countries 라이브러리에서 가져왔음
